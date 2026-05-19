41f8e837e01c88af4cf4ecea6d6551e233e67f9a
Mateus <Mateus@DESKTOP-2I5D6AU>
2026-05-19 20:40:35 -0300

    feat: adicionar thumbnail nas videoaulas

--- EnemPrep.Web/Models/Shared/VideoAulaViewModel.cs
+++ EnemPrep.Web/Models/Shared/VideoAulaViewModel.cs
@@ -12,4 +12,27 @@ public class VideoAulaViewModel
         DuracaoSegundos >= 3600
             ? TimeSpan.FromSeconds(DuracaoSegundos).ToString(@"h\:mm\:ss")
             : TimeSpan.FromSeconds(DuracaoSegundos).ToString(@"m\:ss");
+
+    public string VideoId
+    {
+        get
+        {
+            var url = UrlVideo ?? "";
+            if (url.Contains("youtu.be/"))
+            {
+                return url.Split("youtu.be/").Last().Split("?").First().Split("&").First();
+            }
+            if (url.Contains("v="))
+            {
+                return url.Split("v=").Last().Split("&").First();
+            }
+            if (url.Contains("embed/"))
+            {
+                return url.Split("embed/").Last().Split("?").First();
+            }
+            return string.Empty;
+        }
+    }
+
+    public string ThumbnailUrl => string.IsNullOrEmpty(VideoId) ? "" : $"https://img.youtube.com/vi/{VideoId}/maxresdefault.jpg";
 }
